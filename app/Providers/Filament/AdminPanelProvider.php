014bd34de442c98e1d1fba7200af454539a38a41
Sinyu Jung <jungsinyu@gmail.com>
2026-04-28 04:21:20 +0900

    Apply pending updates

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -35,6 +35,7 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Indigo, // Insights uses Indigo to differentiate from WMS (Amber)
             ])
+            ->darkMode(false)
             ->viteTheme('resources/css/filament/admin/theme.css')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
